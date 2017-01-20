15226da234ebaf070ed5c438ca363756798c8ed3
larsf <lfredriksen@maprtech.com>
2017-01-19 18:59:34 -0800

    revert unneeded dependency gets

--- build.go
+++ build.go
@@ -371,8 +371,6 @@ func gruntBuildArg(task string) []string {
 
 func setup() {
 	runPrint("go", "get", "-v", "github.com/kardianos/govendor")
-	runPrint("go", "get", "-v", "golang.org/x/sync/errgroup")
-	runPrint("go", "get", "-v", "gopkg.in/macaron.v1")
 	runPrint("go", "install", "-v", "./pkg/cmd/grafana-server")
 }
 
