676be2dca526a97cdc6be11ba340d7e938d6ab94
kay delaney <kay@grafana.com>
2019-08-26 13:01:56 +0100

    Makes changes according to provided feedback

--- public/app/features/explore/state/reducers.ts
+++ public/app/features/explore/state/reducers.ts
@@ -245,7 +245,7 @@ export const itemReducer = reducerFactory<ExploreItemState>({} as ExploreItemSta
   .addMapper({
     filter: initializeExploreAction,
     mapper: (state, action): ExploreItemState => {
-      const { containerWidth, eventBridge, queries, range, mode, ui, originPanel } = action.payload;
+      const { containerWidth, eventBridge, queries, range, mode, ui, originPanelId } = action.payload;
       return {
         ...state,
         containerWidth,
@@ -256,7 +256,7 @@ export const itemReducer = reducerFactory<ExploreItemState>({} as ExploreItemSta
         initialized: true,
         queryKeys: getQueryKeys(queries, state.datasourceInstance),
         ...ui,
-        originPanel,
+        originPanelId,
         update: makeInitialUpdateState(),
       };
     },
@@ -284,7 +284,7 @@ export const itemReducer = reducerFactory<ExploreItemState>({} as ExploreItemSta
         mode = supportedModes[0];
       }
 
-      const originPanel = state.urlState && state.urlState.originPanel;
+      const originPanelId = state.urlState && state.urlState.originPanelId;
 
       // Custom components
       const StartPage = datasourceInstance.components.ExploreStartPage;
@@ -304,7 +304,7 @@ export const itemReducer = reducerFactory<ExploreItemState>({} as ExploreItemSta
         queryKeys: [],
         supportedModes,
         mode,
-        originPanel,
+        originPanelId,
       };
     },
   })
