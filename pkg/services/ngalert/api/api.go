8688b10a168bc5e86605f060d2bc3f0d23fc9ae6
Josue Abreu <josue@grafana.com>
2021-03-19 18:55:26 +0000

    Alerting: Fetch configuration from the database and run a notification instance
    Co-Authored-By: Ganesh Vernekar <15064823+codesome@users.noreply.github.com>
    Signed-off-by: Josue Abreu <josue@grafana.com>

--- pkg/services/ngalert/api/api.go
+++ pkg/services/ngalert/api/api.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/grafana/alerting-api/pkg/api"
+
 	"github.com/grafana/grafana/pkg/infra/log"
 
 	"github.com/grafana/grafana/pkg/services/datasourceproxy"
@@ -29,6 +31,10 @@ import (
 // timeNow makes it possible to test usage of time
 var timeNow = time.Now
 
+type Alertmanager interface {
+	ApplyConfig(config *api.PostableUserConfig) error
+}
+
 // API handlers.
 type API struct {
 	Cfg             *setting.Cfg
@@ -38,6 +44,7 @@ type API struct {
 	Schedule        schedule.ScheduleService
 	Store           store.Store
 	DataProxy       *datasourceproxy.DatasourceProxyService
+	Alertmanager    Alertmanager
 }
 
 // RegisterAPIEndpoints registers API handlers
