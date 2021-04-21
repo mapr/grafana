e4cd752f9b7998deefafb770ede0d39bc64241df
Ganesh Vernekar <ganeshvern@gmail.com>
2021-04-21 22:13:59 +0530

    AlertingNG: Add webhook notification channel
    Signed-off-by: Ganesh Vernekar <ganeshvern@gmail.com>

--- pkg/services/ngalert/notifier/channels/webhook_test.go
+++ pkg/services/ngalert/notifier/channels/webhook_test.go
@@ -3,9 +3,7 @@ package channels
 import (
 	"context"
 	"encoding/json"
-	"errors"
 	"net/url"
-	"os"
 	"testing"
 
 	"github.com/prometheus/alertmanager/notify"
@@ -20,7 +18,7 @@ import (
 	"github.com/grafana/grafana/pkg/services/alerting"
 )
 
-func TestPagerdutyNotifier(t *testing.T) {
+func TestWebhookNotifier(t *testing.T) {
 	tmpl, err := template.FromGlobs("templates/default.tmpl")
 	require.NoError(t, err)
 
@@ -28,20 +26,17 @@ func TestPagerdutyNotifier(t *testing.T) {
 	require.NoError(t, err)
 	tmpl.ExternalURL = externalURL
 
-	hostname, err := os.Hostname()
-	require.NoError(t, err)
-
 	cases := []struct {
 		name         string
 		settings     string
 		alerts       []*types.Alert
-		expMsg       *pagerDutyMessage
+		expMsg       *webhookMessage
 		expInitError error
 		expMsgError  error
 	}{
 		{
 			name:     "Default config with one alert",
-			settings: `{"integrationKey": "abcdefgh0123456789"}`,
+			settings: `{"url": "http://localhost/test"}`,
 			alerts: []*types.Alert{
 				{
 					Alert: model.Alert{
@@ -50,40 +45,46 @@ func TestPagerdutyNotifier(t *testing.T) {
 					},
 				},
 			},
-			expMsg: &pagerDutyMessage{
-				RoutingKey:  "abcdefgh0123456789",
-				DedupKey:    "6e3538104c14b583da237e9693b76debbc17f0f8058ef20492e5853096cf8733",
-				Description: "[firing:1]  (val1)",
-				EventAction: "trigger",
-				Payload: &pagerDutyPayload{
-					Summary:   "[FIRING:1]  (val1)",
-					Source:    hostname,
-					Severity:  "critical",
-					Class:     "todo_class",
-					Component: "Grafana",
-					Group:     "todo_group",
-					CustomDetails: map[string]string{
-						"firing":       "Labels:\n - alertname = alert1\n - lbl1 = val1\nAnnotations:\n - ann1 = annv1\nSource: \n",
-						"num_firing":   "1",
-						"num_resolved": "0",
-						"resolved":     "",
+			expMsg: &webhookMessage{
+				Data: &template.Data{
+					Receiver: "my_receiver",
+					Status:   "firing",
+					Alerts: template.Alerts{
+						{
+							Status: "firing",
+							Labels: template.KV{
+								"alertname": "alert1",
+								"lbl1":      "val1",
+							},
+							Annotations: template.KV{
+								"ann1": "annv1",
+							},
+							Fingerprint: "fac0861a85de433a",
+						},
 					},
+					GroupLabels: template.KV{
+						"alertname": "",
+					},
+					CommonLabels: template.KV{
+						"alertname": "alert1",
+						"lbl1":      "val1",
+					},
+					CommonAnnotations: template.KV{
+						"ann1": "annv1",
+					},
+					ExternalURL: "http://localhost",
 				},
-				Client:    "Grafana",
-				ClientURL: "http://localhost",
-				Links:     []pagerDutyLink{{HRef: "http://localhost", Text: "External URL"}},
+				Version:  "1",
+				GroupKey: "alertname",
+				Title:    "[FIRING:1]  (val1)",
+				State:    "alerting",
+				Message:  "\n**Firing**\nLabels:\n - alertname = alert1\n - lbl1 = val1\nAnnotations:\n - ann1 = annv1\nSource: \n\n\n\n\n",
 			},
 			expInitError: nil,
 			expMsgError:  nil,
 		}, {
-			name: "Custom config with multiple alerts",
-			settings: `{
-				"integrationKey": "abcdefgh0123456789",
-				"severity": "warning",
-				"class": "{{ .Status }}",
-				"component": "My Grafana",
-				"group": "my_group"
-			}`,
+			name:     "Custom config with multiple alerts",
+			settings: `{"url": "http://localhost/test"}`,
 			alerts: []*types.Alert{
 				{
 					Alert: model.Alert{
@@ -97,42 +98,54 @@ func TestPagerdutyNotifier(t *testing.T) {
 					},
 				},
 			},
-			expMsg: &pagerDutyMessage{
-				RoutingKey:  "abcdefgh0123456789",
-				DedupKey:    "6e3538104c14b583da237e9693b76debbc17f0f8058ef20492e5853096cf8733",
-				Description: "[firing:2]  ",
-				EventAction: "trigger",
-				Payload: &pagerDutyPayload{
-					Summary:   "[FIRING:2]  ",
-					Source:    hostname,
-					Severity:  "warning",
-					Class:     "firing",
-					Component: "My Grafana",
-					Group:     "my_group",
-					CustomDetails: map[string]string{
-						"firing":       "Labels:\n - alertname = alert1\n - lbl1 = val1\nAnnotations:\n - ann1 = annv1\nSource: \nLabels:\n - alertname = alert1\n - lbl1 = val2\nAnnotations:\n - ann1 = annv2\nSource: \n",
-						"num_firing":   "2",
-						"num_resolved": "0",
-						"resolved":     "",
+			expMsg: &webhookMessage{
+				Data: &template.Data{
+					Receiver: "my_receiver",
+					Status:   "firing",
+					Alerts: template.Alerts{
+						{
+							Status: "firing",
+							Labels: template.KV{
+								"alertname": "alert1",
+								"lbl1":      "val1",
+							},
+							Annotations: template.KV{
+								"ann1": "annv1",
+							},
+							Fingerprint: "fac0861a85de433a",
+						}, {
+							Status: "firing",
+							Labels: template.KV{
+								"alertname": "alert1",
+								"lbl1":      "val2",
+							},
+							Annotations: template.KV{
+								"ann1": "annv2",
+							},
+							Fingerprint: "fab6861a85d5eeb5",
+						},
 					},
+					GroupLabels: template.KV{
+						"alertname": "",
+					},
+					CommonLabels: template.KV{
+						"alertname": "alert1",
+					},
+					CommonAnnotations: template.KV{},
+					ExternalURL:       "http://localhost",
 				},
-				Client:    "Grafana",
-				ClientURL: "http://localhost",
-				Links:     []pagerDutyLink{{HRef: "http://localhost", Text: "External URL"}},
+				Version:  "1",
+				GroupKey: "alertname",
+				Title:    "[FIRING:2]  ",
+				State:    "alerting",
+				Message:  "\n**Firing**\nLabels:\n - alertname = alert1\n - lbl1 = val1\nAnnotations:\n - ann1 = annv1\nSource: \nLabels:\n - alertname = alert1\n - lbl1 = val2\nAnnotations:\n - ann1 = annv2\nSource: \n\n\n\n\n",
 			},
 			expInitError: nil,
 			expMsgError:  nil,
 		}, {
 			name:         "Error in initing",
 			settings:     `{}`,
-			expInitError: alerting.ValidationError{Reason: "Could not find integration key property in settings"},
-		}, {
-			name: "Error in building message",
-			settings: `{
-				"integrationKey": "abcdefgh0123456789",
-				"class": "{{ .Status }"
-			}`,
-			expMsgError: errors.New("build pagerduty message: failed to template PagerDuty message: template: :1: unexpected \"}\" in operand"),
+			expInitError: alerting.ValidationError{Reason: "Could not find url property in settings"},
 		},
 	}
 
@@ -142,12 +155,12 @@ func TestPagerdutyNotifier(t *testing.T) {
 			require.NoError(t, err)
 
 			m := &models.AlertNotification{
-				Name:     "pageduty_testing",
-				Type:     "pagerduty",
+				Name:     "webhook_testing",
+				Type:     "webhook",
 				Settings: settingsJSON,
 			}
 
-			pn, err := NewPagerdutyNotifier(m, tmpl)
+			pn, err := NewWebHookNotifier(m, tmpl)
 			if c.expInitError != nil {
 				require.Error(t, err)
 				require.Equal(t, c.expInitError.Error(), err.Error())
@@ -163,6 +176,7 @@ func TestPagerdutyNotifier(t *testing.T) {
 
 			ctx := notify.WithGroupKey(context.Background(), "alertname")
 			ctx = notify.WithGroupLabels(ctx, model.LabelSet{"alertname": ""})
+			ctx = notify.WithReceiverName(ctx, "my_receiver")
 			ok, err := pn.Notify(ctx, c.alerts...)
 			if c.expMsgError != nil {
 				require.False(t, ok)
@@ -170,8 +184,8 @@ func TestPagerdutyNotifier(t *testing.T) {
 				require.Equal(t, c.expMsgError.Error(), err.Error())
 				return
 			}
-			require.True(t, ok)
 			require.NoError(t, err)
+			require.True(t, ok)
 
 			expBody, err := json.Marshal(c.expMsg)
 			require.NoError(t, err)
