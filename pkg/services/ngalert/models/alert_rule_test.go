816b7aa97efcddd76322131aee6e4ce6b7eaf0cf
Yuriy Tseretyan <yuriy.tseretyan@grafana.com>
2022-02-24 19:00:11 -0500

    add tests

--- pkg/services/ngalert/models/alert_rule_test.go
+++ pkg/services/ngalert/models/alert_rule_test.go
@@ -1,15 +1,20 @@
 package models
 
 import (
+	"encoding/json"
+	"fmt"
 	"math/rand"
+	"reflect"
 	"strings"
 	"testing"
 	"time"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/grafana/grafana/pkg/util"
+	"github.com/grafana/grafana/pkg/util/cmputil"
 )
 
 func TestNoDataStateFromString(t *testing.T) {
@@ -224,4 +229,487 @@ func TestPatchPartialAlertRule(t *testing.T) {
 			})
 		}
 	})
+}
+
+func TestDiff(t *testing.T) {
+	simpleDiff := func(field string) func(t *testing.T, orig, change *AlertRule, d *cmputil.DiffReport) {
+		return func(t *testing.T, orig, change *AlertRule, d *cmputil.DiffReport) {
+			result := false
+			if assert.Len(t, d.Diffs, 1) {
+				diff := d.Diffs[0]
+				result = assert.Equal(t, field, diff.Path)
+
+				origVal := reflect.Indirect(reflect.ValueOf(orig))
+				changeVal := reflect.Indirect(reflect.ValueOf(change))
+
+				origField := origVal.FieldByName(field)
+				changeField := changeVal.FieldByName(field)
+
+				if origField.Kind() == reflect.Ptr {
+					checkPtr := func(expected, actual reflect.Value) bool {
+						if expected.IsNil() {
+							return assert.Nil(t, actual.Interface())
+						} else {
+							if !assert.NotNil(t, actual.Interface()) {
+								return false
+							}
+							if actual.Kind() == reflect.Ptr {
+								actual = actual.Elem()
+							}
+							return assert.Equal(t, expected.Elem().Interface(), actual.Interface())
+						}
+					}
+					result = result && checkPtr(origField, diff.Left)
+					result = result && checkPtr(changeField, diff.Right)
+				} else {
+					result = result && assert.Equal(t, origField.Interface(), diff.Left.Interface())
+					result = result && assert.Equal(t, changeField.Interface(), diff.Right.Interface())
+				}
+			}
+
+			if !result {
+				t.Logf("rule1: %#v, rule2: %#v\ndiff: %s", orig, change, d)
+			}
+		}
+	}
+
+	testCases := []struct {
+		name     string
+		copyRule func(r *AlertRule) *AlertRule
+		assert   func(t *testing.T, orig, change *AlertRule, d *cmputil.DiffReport)
+	}{
+		{
+			name: "should not be diff if same fields",
+			copyRule: func(r *AlertRule) *AlertRule {
+				return CopyRule(r)
+			},
+			assert: func(t *testing.T, orig, change *AlertRule, d *cmputil.DiffReport) {
+				if d != nil {
+					t.Fatalf("expected nil but got: %s", d)
+				}
+			},
+		},
+		{
+			name: "should detect changes in ID",
+			copyRule: func(r *AlertRule) *AlertRule {
+				rule := CopyRule(r)
+				for r.ID == rule.ID {
+					rule.ID = rand.Int63()
+				}
+				return rule
+			},
+			assert: simpleDiff("ID"),
+		},
+		{
+			name: "should detect changes in OrgID",
+			copyRule: func(r *AlertRule) *AlertRule {
+				rule := CopyRule(r)
+				for r.OrgID == rule.OrgID {
+					rule.OrgID = rand.Int63()
+				}
+				return rule
+			},
+			assert: simpleDiff("OrgID"),
+		},
+		{
+			name: "should detect changes in Title",
+			copyRule: func(r *AlertRule) *AlertRule {
+				rule := CopyRule(r)
+				for r.Title == rule.Title {
+					r.Title = util.GenerateShortUID()
+				}
+				return rule
+			},
+			assert: simpleDiff("Title"),
+		},
+		{
+			name: "should detect changes in Condition",
+			copyRule: func(r *AlertRule) *AlertRule {
+				rule := CopyRule(r)
+				for r.Condition == rule.Condition {
+					r.Condition = util.GenerateShortUID()
+				}
+				return rule
+			},
+			assert: simpleDiff("Condition"),
+		},
+		{
+			name: "should detect changes in Updated",
+			copyRule: func(r *AlertRule) *AlertRule {
+				rule := CopyRule(r)
+				for r.Updated == rule.Updated {
+					r.Updated = r.Updated.Add(time.Duration(rand.Int()))
+				}
+				return rule
+			},
+			assert: simpleDiff("Updated"),
+		},
+		{
+			name: "should detect changes in IntervalSeconds",
+			copyRule: func(r *AlertRule) *AlertRule {
+				rule := CopyRule(r)
+				for r.IntervalSeconds == rule.IntervalSeconds {
+					r.IntervalSeconds = rand.Int63()
+				}
+				return rule
+			},
+			assert: simpleDiff("IntervalSeconds"),
+		},
+		{
+			name: "should detect changes in Version",
+			copyRule: func(r *AlertRule) *AlertRule {
+				rule := CopyRule(r)
+				for r.Version == rule.Version {
+					r.Version = rand.Int63()
+				}
+				return rule
+			},
+			assert: simpleDiff("Version"),
+		},
+		{
+			name: "should detect changes in UID",
+			copyRule: func(r *AlertRule) *AlertRule {
+				rule := CopyRule(r)
+				for r.UID == rule.UID {
+					r.UID = util.GenerateShortUID()
+				}
+				return rule
+			},
+			assert: simpleDiff("UID"),
+		},
+		{
+			name: "should detect changes in NamespaceUID",
+			copyRule: func(r *AlertRule) *AlertRule {
+				rule := CopyRule(r)
+				for r.NamespaceUID == rule.NamespaceUID {
+					r.NamespaceUID = util.GenerateShortUID()
+				}
+				return rule
+			},
+			assert: simpleDiff("NamespaceUID"),
+		},
+		{
+			name: "should detect changes in DashboardUID",
+			copyRule: func(r *AlertRule) *AlertRule {
+				rule := CopyRule(r)
+				if r.DashboardUID == nil {
+					d := util.GenerateShortUID()
+					rule.DashboardUID = &d
+				} else if rand.Int63()%2 == 0 {
+					rule.DashboardUID = nil
+				} else {
+					d := util.GenerateShortUID()
+					for d == *r.DashboardUID {
+						d = util.GenerateShortUID()
+					}
+					rule.DashboardUID = &d
+				}
+				return rule
+			},
+			assert: simpleDiff("DashboardUID"),
+		},
+		{
+			name: "should detect changes in PanelID",
+			copyRule: func(r *AlertRule) *AlertRule {
+				rule := CopyRule(r)
+				if r.PanelID == nil {
+					d := rand.Int63()
+					rule.PanelID = &d
+				} else if rand.Int63()%2 == 0 {
+					r.PanelID = nil
+				} else {
+					d := rand.Int63()
+					for d == *r.PanelID {
+						d = rand.Int63()
+					}
+					rule.PanelID = &d
+				}
+				return rule
+			},
+			assert: simpleDiff("PanelID"),
+		},
+		{
+			name: "should detect changes in RuleGroup",
+			copyRule: func(r *AlertRule) *AlertRule {
+				rule := CopyRule(r)
+				for r.RuleGroup == rule.RuleGroup {
+					r.RuleGroup = util.GenerateShortUID()
+				}
+				return rule
+			},
+			assert: simpleDiff("RuleGroup"),
+		},
+		{
+			name: "should detect changes in NoDataState",
+			copyRule: func(r *AlertRule) *AlertRule {
+				rule := CopyRule(r)
+				for r.NoDataState == rule.NoDataState {
+					rule.NoDataState = NoDataState(util.GenerateShortUID())
+				}
+				return rule
+			},
+			assert: simpleDiff("NoDataState"),
+		},
+		{
+			name: "should detect changes in ExecErrState",
+			copyRule: func(r *AlertRule) *AlertRule {
+				rule := CopyRule(r)
+				for r.ExecErrState == rule.ExecErrState {
+					rule.ExecErrState = ExecutionErrorState(util.GenerateShortUID())
+				}
+				return rule
+			},
+			assert: simpleDiff("ExecErrState"),
+		},
+		{
+			name: "should detect changes in For",
+			copyRule: func(r *AlertRule) *AlertRule {
+				rule := CopyRule(r)
+				for r.For == rule.For {
+					rule.For = time.Duration(rand.Int63())
+				}
+				return rule
+			},
+			assert: simpleDiff("For"),
+		},
+		{
+			name: "should detect changes in Annotation values",
+			copyRule: func(r *AlertRule) *AlertRule {
+				if len(r.Annotations) == 0 {
+					r.Annotations = make(map[string]string, 8)
+					for i := 0; i < rand.Intn(5)+3; i++ {
+						r.Annotations[util.GenerateShortUID()] = util.GenerateShortUID()
+					}
+				}
+				rule := CopyRule(r)
+				for key, value := range rule.Annotations {
+					v := util.GenerateShortUID()
+					for v == value {
+						v = util.GenerateShortUID()
+					}
+					rule.Annotations[key] = v
+				}
+				return rule
+			},
+			assert: func(t *testing.T, orig, change *AlertRule, d *cmputil.DiffReport) {
+				result := false
+				if assert.Len(t, d.Diffs, len(orig.Annotations)) {
+					result = true
+				outerloop:
+					for key, expected := range orig.Annotations {
+						expectedKey := fmt.Sprintf("Annotations[%s]", key)
+						expectedNew := change.Annotations[key]
+						for _, diff := range d.Diffs {
+							if diff.Path == expectedKey {
+								result = result && assert.Equal(t, expected, diff.Left.String())
+								result = result && assert.Equal(t, expectedNew, diff.Right.String())
+								continue outerloop
+							}
+						}
+						result = result && assert.Fail(t, "path %s does not exist in diff", expectedKey)
+					}
+				}
+				if !result {
+					t.Logf("rule1: %#v, rule2: %#v\ndiff: %s", orig, change, d)
+				}
+			},
+		},
+		{
+			name: "should detect new keys in Annotation",
+			copyRule: func(r *AlertRule) *AlertRule {
+				if len(r.Annotations) == 0 {
+					r.Annotations = make(map[string]string, 8)
+					for i := 0; i < rand.Intn(5)+3; i++ {
+						r.Annotations[util.GenerateShortUID()] = util.GenerateShortUID()
+					}
+				}
+				rule := CopyRule(r)
+				for i := 0; i < rand.Intn(5)+1; i++ {
+					key := util.GenerateShortUID()
+					for _, ok := rule.Annotations[key]; ok; {
+						key = util.GenerateShortUID()
+					}
+					rule.Annotations[key] = util.GenerateShortUID()
+				}
+				return rule
+			},
+			assert: func(t *testing.T, orig, change *AlertRule, d *cmputil.DiffReport) {
+				result := false
+				t.Logf("rule1: %#v, rule2: %#v\ndiff: %s", orig, change, d)
+
+				if assert.Len(t, d.Diffs, len(change.Annotations)-len(orig.Annotations)) {
+					result = true
+				outerloop:
+					for key, expectedNew := range change.Annotations {
+						if _, ok := orig.Annotations[key]; ok {
+							continue
+						}
+						expectedKey := fmt.Sprintf("Annotations[%s]", key)
+						for _, diff := range d.Diffs {
+							if diff.Path == expectedKey {
+								result = result && assert.False(t, diff.Left.IsValid())
+								result = result && assert.Equal(t, expectedNew, diff.Right.String())
+								continue outerloop
+							}
+						}
+						result = result && assert.Fail(t, "path %s does not exist in diff", expectedKey)
+					}
+				}
+				if !result {
+					t.Logf("rule1: %#v, rule2: %#v\ndiff: %s", orig, change, d)
+				}
+			},
+		},
+		{
+			name: "should detect changes in Labels values",
+			copyRule: func(r *AlertRule) *AlertRule {
+				if len(r.Labels) == 0 {
+					r.Labels = make(map[string]string, 8)
+					for i := 0; i < rand.Intn(5)+3; i++ {
+						r.Labels[util.GenerateShortUID()] = util.GenerateShortUID()
+					}
+				}
+				rule := CopyRule(r)
+				for key, value := range rule.Labels {
+					v := util.GenerateShortUID()
+					for v == value {
+						v = util.GenerateShortUID()
+					}
+					rule.Labels[key] = v
+				}
+				return rule
+			},
+			assert: func(t *testing.T, orig, change *AlertRule, d *cmputil.DiffReport) {
+				result := false
+				if assert.Len(t, d.Diffs, len(orig.Labels)) {
+					result = true
+				outerloop:
+					for key, expected := range orig.Labels {
+						expectedKey := fmt.Sprintf("Labels[%s]", key)
+						expectedNew := change.Labels[key]
+						for _, diff := range d.Diffs {
+							if diff.Path == expectedKey {
+								result = result && assert.Equal(t, expected, diff.Left.String())
+								result = result && assert.Equal(t, expectedNew, diff.Right.String())
+								continue outerloop
+							}
+						}
+						result = result && assert.Fail(t, "path %s does not exist in diff", expectedKey)
+					}
+				}
+				if !result {
+					t.Logf("rule1: %#v, rule2: %#v\ndiff: %s", orig, change, d)
+				}
+			},
+		},
+		{
+			name: "should detect new keys in Labels",
+			copyRule: func(r *AlertRule) *AlertRule {
+				if len(r.Labels) == 0 {
+					r.Labels = make(map[string]string, 8)
+					for i := 0; i < rand.Intn(5)+3; i++ {
+						r.Labels[util.GenerateShortUID()] = util.GenerateShortUID()
+					}
+				}
+				rule := CopyRule(r)
+				for i := 0; i < rand.Intn(5)+1; i++ {
+					key := util.GenerateShortUID()
+					for _, ok := rule.Labels[key]; ok; {
+						key = util.GenerateShortUID()
+					}
+					rule.Labels[key] = util.GenerateShortUID()
+				}
+				return rule
+			},
+			assert: func(t *testing.T, orig, change *AlertRule, d *cmputil.DiffReport) {
+				result := false
+				t.Logf("rule1: %#v, rule2: %#v\ndiff: %s", orig, change, d)
+
+				if assert.Len(t, d.Diffs, len(change.Labels)-len(orig.Labels)) {
+					result = true
+				outerloop:
+					for key, expectedNew := range change.Labels {
+						if _, ok := orig.Labels[key]; ok {
+							continue
+						}
+						expectedKey := fmt.Sprintf("Labels[%s]", key)
+						for _, diff := range d.Diffs {
+							if diff.Path == expectedKey {
+								result = result && assert.False(t, diff.Left.IsValid())
+								result = result && assert.Equal(t, expectedNew, diff.Right.String())
+								continue outerloop
+							}
+						}
+						result = result && assert.Fail(t, "path %s does not exist in diff", expectedKey)
+					}
+				}
+				if !result {
+					t.Logf("rule1: %#v, rule2: %#v\ndiff: %s", orig, change, d)
+				}
+			},
+		},
+		{
+			name: "should detect changes in Data",
+			copyRule: func(r *AlertRule) *AlertRule {
+				rule := CopyRule(r)
+				rule.Data = append(rule.Data, GenerateAlertQuery())
+				rule.Data = append([]AlertQuery{
+					GenerateAlertQuery(),
+				}, rule.Data...)
+
+				return rule
+			},
+			assert: func(t *testing.T, orig, change *AlertRule, d *cmputil.DiffReport) {
+				result := false
+				if !result {
+					t.Logf("rule1: %#v, rule2: %#v\ndiff: %s", orig, change, d)
+				}
+			},
+		},
+		{
+			name: "should detect changes in Data",
+			copyRule: func(r *AlertRule) *AlertRule {
+				rule := CopyRule(r)
+				r.Data = append(r.Data, GenerateAlertQuery())
+				rule.Data = append([]AlertQuery{
+					GenerateAlertQuery(),
+				}, rule.Data...)
+
+				return rule
+			},
+			assert: func(t *testing.T, orig, change *AlertRule, d *cmputil.DiffReport) {
+				result := false
+				if !result {
+					t.Logf("rule1: %#v, rule2: %#v\ndiff: %s", orig, change, d)
+				}
+			},
+		},
+		{
+			name: "should detect changes in Data",
+			copyRule: func(r *AlertRule) *AlertRule {
+				r.Data = append(r.Data, GenerateAlertQuery(), GenerateAlertQuery())
+				rule := CopyRule(r)
+				rule.Data[0].QueryType = util.GenerateShortUID()
+				// rule.Data[0].RefID = util.GenerateShortUID()
+				rule.Data[0].DatasourceUID = util.GenerateShortUID()
+				rule.Data[0].Model = json.RawMessage(`{ "test": "test" }`)
+				return rule
+			},
+			assert: func(t *testing.T, orig, change *AlertRule, d *cmputil.DiffReport) {
+				result := false
+				if !result {
+					t.Logf("rule1: %#v, rule2: %#v\ndiff: %s", orig, change, d)
+				}
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			rule1 := AlertRuleGen()()
+			rule2 := testCase.copyRule(rule1)
+			result := rule1.Diff(rule2)
+			testCase.assert(t, rule1, rule2, result)
+		})
+	}
 }
