c71ee3ebf737ed54ade2fbc9d2e99f3888309bb9
Karl Persson <kalle.persson@grafana.com>
2022-08-10 14:39:17 +0200

    explicitly disable rbac in tests

--- pkg/services/publicdashboards/api/api_test.go
+++ pkg/services/publicdashboards/api/api_test.go
@@ -39,6 +39,7 @@ import (
 func TestAPIGetPublicDashboard(t *testing.T) {
 	t.Run("It should 404 if featureflag is not enabled", func(t *testing.T) {
 		cfg := setting.NewCfg()
+		cfg.RBACEnabled = false
 		qs := buildQueryDataService(t, nil, nil, nil)
 		service := publicdashboards.NewFakePublicDashboardService(t)
 		service.On("GetPublicDashboard", mock.Anything, mock.AnythingOfType("string")).
@@ -96,9 +97,12 @@ func TestAPIGetPublicDashboard(t *testing.T) {
 			service.On("GetPublicDashboard", mock.Anything, mock.AnythingOfType("string")).
 				Return(test.PublicDashboardResult, test.PublicDashboardErr).Maybe()
 
+			cfg := setting.NewCfg()
+			cfg.RBACEnabled = false
+
 			testServer := setupTestServer(
 				t,
-				setting.NewCfg(),
+				cfg,
 				buildQueryDataService(t, nil, nil, nil),
 				featuremgmt.WithFeatures(featuremgmt.FlagPublicDashboards),
 				service,
@@ -173,9 +177,12 @@ func TestAPIGetPublicDashboardConfig(t *testing.T) {
 			service.On("GetPublicDashboardConfig", mock.Anything, mock.AnythingOfType("int64"), mock.AnythingOfType("string")).
 				Return(test.PublicDashboardResult, test.PublicDashboardErr)
 
+			cfg := setting.NewCfg()
+			cfg.RBACEnabled = false
+
 			testServer := setupTestServer(
 				t,
-				setting.NewCfg(),
+				cfg,
 				buildQueryDataService(t, nil, nil, nil),
 				featuremgmt.WithFeatures(featuremgmt.FlagPublicDashboards),
 				service,
@@ -237,9 +244,12 @@ func TestApiSavePublicDashboardConfig(t *testing.T) {
 			service.On("SavePublicDashboardConfig", mock.Anything, mock.AnythingOfType("*models.SavePublicDashboardConfigDTO")).
 				Return(&PublicDashboard{IsEnabled: true}, test.SaveDashboardErr)
 
+			cfg := setting.NewCfg()
+			cfg.RBACEnabled = false
+
 			testServer := setupTestServer(
 				t,
-				setting.NewCfg(),
+				cfg,
 				buildQueryDataService(t, nil, nil, nil),
 				featuremgmt.WithFeatures(featuremgmt.FlagPublicDashboards),
 				service,
@@ -305,10 +315,12 @@ func TestAPIQueryPublicDashboard(t *testing.T) {
 
 	setup := func(enabled bool) (*web.Mux, *publicdashboards.FakePublicDashboardService) {
 		service := publicdashboards.NewFakePublicDashboardService(t)
+		cfg := setting.NewCfg()
+		cfg.RBACEnabled = false
 
 		testServer := setupTestServer(
 			t,
-			setting.NewCfg(),
+			cfg,
 			qds,
 			featuremgmt.WithFeatures(featuremgmt.FlagPublicDashboards, enabled),
 			service,
@@ -547,13 +559,15 @@ func TestIntegrationUnauthenticatedUserCanGetPubdashPanelQueryData(t *testing.T)
 
 	// create public dashboard
 	store := publicdashboardsStore.ProvideStore(db)
-	service := publicdashboardsService.ProvideService(setting.NewCfg(), store)
+	cfg := setting.NewCfg()
+	cfg.RBACEnabled = false
+	service := publicdashboardsService.ProvideService(cfg, store)
 	pubdash, err := service.SavePublicDashboardConfig(context.Background(), savePubDashboardCmd)
 	require.NoError(t, err)
 
 	// setup test server
 	server := setupTestServer(t,
-		setting.NewCfg(),
+		cfg,
 		qds,
 		featuremgmt.WithFeatures(featuremgmt.FlagPublicDashboards),
 		service,
