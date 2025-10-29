024a0b6b6638b023429354d244e6ff4f95512061
laurenashleigh <lauren.armstrong@grafana.com>
2025-10-29 09:53:50 +0000

    add feature toggle to auto gen files

--- packages/grafana-data/src/types/featureToggles.gen.ts
+++ packages/grafana-data/src/types/featureToggles.gen.ts
@@ -224,6 +224,10 @@ export interface FeatureToggles {
   */
   externalServiceAccounts?: boolean;
   /**
+  * Enables creating alert rules from a panel using a drawer UI
+  */
+  createAlertRuleFromPanel?: boolean;
+  /**
   * Enables native HTTP Histograms
   */
   enableNativeHTTPHistogram?: boolean;
@@ -797,10 +801,6 @@ export interface FeatureToggles {
   * Enables less memory intensive Elasticsearch result parsing
   */
   elasticsearchImprovedParsing?: boolean;
-  /**
-   * Enables creating alert rules from a panel using a drawer UI
-   */
-  createAlertRuleFromPanel?: boolean;
   /**
   * Shows defined connections for a data source in the plugins detail page
   */
