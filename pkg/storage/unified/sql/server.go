c4ade47cff862ca5c490491d0bc07e140749e0a7
Mustafa Sencer Özcan <mustafasencer.ozcan@grafana.com>
2025-11-13 17:28:01 +0100

    fix: remove some tbis

--- pkg/storage/unified/sql/server.go
+++ pkg/storage/unified/sql/server.go
@@ -117,7 +117,6 @@ func NewResourceServer(opts ServerOptions) (resource.ResourceServer, error) {
 		serverOptions.Diagnostics = backend
 		serverOptions.Lifecycle = backend
 	}
-	// Add Datamigration after starting server
 
 	serverOptions.Search = opts.SearchOptions
 	serverOptions.IndexMetrics = opts.IndexMetrics
