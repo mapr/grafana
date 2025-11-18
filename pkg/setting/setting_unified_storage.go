eed5ba008956fb5f961d696e180a600efe618149
Rafael Paulovic <rafael.paulovic@grafana.com>
2025-11-18 09:50:53 +0100

    chore: enforce EnableSearch when running migrations

--- pkg/setting/setting_unified_storage.go
+++ pkg/setting/setting_unified_storage.go
@@ -62,9 +62,11 @@ func (cfg *Cfg) setUnifiedStorageConfig() {
 				DualWriterMigrationDataSyncDisabled: true,
 			}
 		}
+		cfg.EnableSearch = section.Key("enable_search").MustBool(true)
+	} else {
+		cfg.EnableSearch = section.Key("enable_search").MustBool(false)
 	}
 
-	cfg.EnableSearch = section.Key("enable_search").MustBool(false)
 	cfg.MaxPageSizeBytes = section.Key("max_page_size_bytes").MustInt(0)
 	cfg.IndexPath = section.Key("index_path").String()
 	cfg.IndexWorkers = section.Key("index_workers").MustInt(10)
